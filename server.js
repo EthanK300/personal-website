8112763e9f8b6a873e4cdd27f7ef65f8c2332767
Ethan <1bluejellybean@gmail.com>
2025-04-06 18:59:42 -0400

    contact working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const app = express();
+const fs = require("fs");
 dotenv.config();
 
 const { Client } = require("@notionhq/client");
@@ -12,6 +13,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 const IP = process.env.IP;
+const ACCEPT = process.env.ACCEPT;
 
 const listener = app.listen(PORT, IP, () => {
     console.log(`server running on port ${PORT} on ip ${IP}`);
@@ -53,11 +55,26 @@ app.get("/status", async function(req, res) {
 });
 
 app.post("/contactForm", async function(req, res) {
+    let content = "";
     console.log("receiving contact form submission data");
     console.log(req.body);
+    content = "New message from " + req.body.fname + ", " + req.body.lname + "\n\n" + "Subject: " + req.body.subject + "\n\n" + req.body.content + "\n\n"; 
+    console.log(content);
+    if(ACCEPT == "yes"){
+        console.log("writing message to file");
+        fs.writeFile('./tempFormSubmissions.txt', content, { flag: 'a+' }, err => {});
+        res.status(200).send({
+            success:true,
+            message:"ok",
+            body: "Message recieved.",
+        });
+    }else{
+        console.log("message rejected");
+        res.status(200).send({
+            success:true,
+            message:"ok",
+            body: "Sorry, I'm not accepting messages right now!"
+        });
+    }
 
-    res.status(200).send({
-        success:true,
-        message:"ok"
-    });
 });
